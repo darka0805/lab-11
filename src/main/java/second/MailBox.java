777c8cbd5379f4b502e97be62c335f42daa6b37b
darka0805 <kuzyshyn.pn@ucu.edu.ua>
2023-12-26 13:46:45 +0200

    new

--- src/main/java/second/MailBox.java
+++ src/main/java/second/MailBox.java
@@ -3,20 +3,20 @@ package second;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MailBox {
+public class MailBox { 
     private MailSender mailSender;
     private List<MailInfo> infos;
 
-    public MailBox() {
+    public MailBox() { 
         mailSender = new MailSender();
         infos = new ArrayList<>();
-    }
+    } 
 
-    public void addMailInfo(MailInfo mailInfo) {infos.add(mailInfo);}
+    public void addMailInfo(MailInfo mailInfo) {infos.add(mailInfo) ; }
 
     public void sendAll() {
         for (MailInfo letter: infos) {
             mailSender.sendMail(letter);
-        }
-    }
-}
+        } 
+    } 
+} 
